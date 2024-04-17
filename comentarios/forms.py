cffd8338bd8a43664c2c4193b517fd3822a2c6b7
MildePaula <millenadepaula@gmail.com>
2024-04-17 13:31:07 -0300

    Limpar conteudo

--- comentarios/forms.py
+++
@@ -1,9 +0,0 @@
-# CRIAR O ARQUIVO forms.py 
-# CÓDIGO DO FORM
-from django import forms
-from .models import Comentario
-
-class ComentarioForm(forms.ModelForm):
-    class Meta:
-        model = Comentario
-        fields = ['texto']
--- noticia/forms.py
+++
@@ -1,8 +0,0 @@
-
-from django import forms
-from .models import Noticia
-
-class NoticiaForm(forms.ModelForm):
-    class Meta:
-        model = Noticia
-        fields = ['titulo', 'conteudo']
