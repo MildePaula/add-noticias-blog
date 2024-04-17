d81324947857f92ba183a3396556db04dc4ceaa7
MildePaula <millenadepaula@gmail.com>
2024-04-17 12:42:21 -0300

    MOD1

--- noticia/models.py
+++ noticia/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Noticia(models.Model):
     titulo = models.CharField(max_length=200)
-    conteudo = models.TextField()
+    cont_eudo = models.TextField()
     data_p = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
